b1f9a59b79466cabfd461db37c21f831f3236063
asasso <sassoantonio14@gmail.com>
2022-03-16 16:27:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<!--
+An example of creating a reusable grid component and using it with external data.
+-->
+
+<script setup>
+// import demogrid from './Grid.vue'
+import Logo from './Logo.vue'
+import { ref } from 'vue'
+import './css/main.css'
+
+</script>
+
+<template>
+ <Logo class="logo" />
+
+</template>
---
+++ src/Logo.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const props = defineProps({
+  larghezza: String,
+  lunghezza: String
+})
+
+</script>
+
+<template>
+
+<img src="./images/prototipo-3.svg" />
+</template>
